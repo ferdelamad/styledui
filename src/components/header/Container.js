546c73a63386ad5472b5bbf2fc25b1d2dc078fc5
Fernando <ferdelamad@gmail.com>
2019-01-01 20:37:41 -0800

    Added Header element 🤙

---
+++ src/components/header/Container.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+  position: absolute;
+  padding: 30px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: red;
+`
+
+export default Container
