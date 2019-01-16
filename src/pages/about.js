25efc0de3350e5d84a5ecb85d46b65842e2c26f8
Fernando <ferdelamad@gmail.com>
2019-01-15 22:55:22 -0800

    Created sections pages

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const About = () => (
+  <Layout>
+    <SEO title="Styled UI About" />
+    <h1>About page</h1>
+  </Layout>
+)
+
+export default About
